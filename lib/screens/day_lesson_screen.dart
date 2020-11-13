6e67f946abfff7714623fc7f7d66bc2e904bea2d
Gabriel <gabriel.elbaz@free.fr>
2020-11-13 15:04:38 +0200

    Fix DayLessonScreen Platform

--- lib/screens/day_lesson_screen.dart
+++ lib/screens/day_lesson_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -84,7 +86,7 @@ class _DayLessonScreenState extends State<DayLessonScreen> {
   Widget build(BuildContext context) {
     _init();
 
-    return getAndroidPage();
+    return (Platform.isIOS) ? getiOSPage() : getAndroidPage();
   }
 
   Widget getiOSPage() {
